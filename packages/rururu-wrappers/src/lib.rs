21ffe994116b1eaa721a24a87dd363a55a631222
Artem Shloyda <artemshloida@yandex.ru>
2026-01-19 22:12:32 +0300

    Remove FFmpeg, Assimp, and OpenEXR dependencies from CI workflow and make features optional
    - Remove FFmpeg, Assimp, OpenEXR, clang, and cmake from CI apt dependencies
    - Exclude rururu-file-handler, rururu-wrappers, and rururu-files from workspace CI jobs
    - Disable ffmpeg feature by default in rururu-file-handler
    - Make openexr and assimp optional features in rururu-wrappers with feature gates
    - Add full feature flag to rururu-wrappers for enabling all optional dependencies

--- packages/rururu-wrappers/src/lib.rs
+++ packages/rururu-wrappers/src/lib.rs
@@ -1,7 +1,15 @@
 pub mod color;
+
+#[cfg(feature = "openexr")]
 pub mod exr;
+
+#[cfg(feature = "assimp")]
 pub mod model3d;
 
 pub use color::ColorManager;
+
+#[cfg(feature = "openexr")]
 pub use exr::{ExrImage, ExrMetadata};
+
+#[cfg(feature = "assimp")]
 pub use model3d::{Model3D, ModelInfo};
